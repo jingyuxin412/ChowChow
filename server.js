9c065483cc9765e750badac31ff0fff8a0e8a484
jingyuxin98 <jingyuxin@email.arizona.edu>
2021-08-03 04:15:19 +0800

    update filename

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-var express=require("express");
-var app=express();
-var cookieParser=require("cookie-parser")
-var session=require("express-session");
-var hbs = require("hbs");
- 
+var  express=require("express");
+var  app=express();
+var  cookieParser=require("cookie-parser")
+var  session=require("express-session");
+
 var routers=require("./routers/route.js")
- 
+
 app.use(cookieParser())
 app.use(session({
     name:'hohoID',
@@ -14,55 +13,37 @@ app.use(session({
     resave:true,//每次请求是否重写设置一份新的session
     saveUninitialized:false//无论有没有session cookie，每次请求都设置个session cookie
 }))
- 
-//模板引擎
-app.set('view engine', 'html');
-app.set('views', __dirname+'/views');
- 
-//运行hbs模块
-app.engine('html', hbs.__express);
-//设置静态资源
- 
-// app.use(express.static('public'));
-// app.use(express.static('avatar'));
-// app.use(express.static('views'));
-app.use(express.static('public_html'));
- 
- 
- //路由函数
- //路由表-----------------------------------------------
+
+app.use(express.static('./public_html'));
+app.use(express.static('./avatar'));
+
 app.all("*",function(req,res,next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
     next()
 })
- app.get('/',routers.index);
- app.get("/web/islogin",routers.islogin);
- 
- //用户相关
- app.post("/user/login",routers.login)
- app.post("/user/regist",routers.regist);
- app.get("/user/logout",routers.logout);
- app.get("/user/findUser",routers.findUser)
- app.get("/user/getAllUser",routers.getAllUser);
- app.post("/user/updateUser",routers.updateUser);
- app.get("/user/delUser",routers.delUser);
- 
- //帖子相关
- app.post("/post/setPost",routers.setPost)
- app.get("/post/getAllPost",routers.getAllPost)
- app.get("/post/getAllPostCount",routers.getAllPostCount);
- app.get("/post/updatePost",routers.updatePost)
- app.get("/post/delPost",routers.delPost)
- app.get("/post/getPersonPost",routers.getPersonPost)
- app.get("/post/getPostByTitle",routers.getPostByTitle)
- app.get("/post/getDetailPost",routers.getDetailPost);
- app.get("/post/setComment",routers.setComment);
- app.get("/post/showUserAllComment",routers.showUserAllComment);
- app.get("/post/showUserNewComment",routers.showUserNewComment);
- app.get("/post/readComment",routers.readComment);
- 
- 
- 
- //监听3000端口
- app.listen(8888);
+app.get('/',routers.index);
+app.get("/web/islogin",routers.islogin);
+
+app.post("/user/login",routers.login)
+app.post("/user/regist",routers.regist);
+app.get("/user/logout",routers.logout);
+app.get("/user/findUser",routers.findUser)
+app.get("/user/getAllUser",routers.getAllUser);
+app.post("/user/updateUser",routers.updateUser);
+app.get("/user/delUser",routers.delUser);
+
+app.post("/post/setPost",routers.setPost)
+app.get("/post/getAllPost",routers.getAllPost)
+app.get("/post/getAllPostCount",routers.getAllPostCount);
+app.get("/post/updatePost",routers.updatePost)
+app.get("/post/delPost",routers.delPost)
+app.get("/post/getPersonPost",routers.getPersonPost)
+app.get("/post/getPostByTitle",routers.getPostByTitle)
+app.get("/post/getDetailPost",routers.getDetailPost);
+app.get("/post/setComment",routers.setComment);
+app.get("/post/showUserAllComment",routers.showUserAllComment);
+app.get("/post/showUserNewComment",routers.showUserNewComment);
+app.get("/post/readComment",routers.readComment);
+
+app.listen(8888);
